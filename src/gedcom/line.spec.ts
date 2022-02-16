ad4d22213a3e48cc7c70b337eee4453a354e2ddc
Jeff VanDyke <jeffvd.a216@gmail.com>
2022-02-16 00:26:14 -0500

    Group line spec tests in their own describe blocks

--- src/gedcom/line.spec.ts
+++ src/gedcom/line.spec.ts
@@ -1,70 +1,74 @@
 import { lineToString, lineFromString } from './line';
 
 describe('gedcom/line ', () => {
-    it('Should write a line from tag and value', () => {
-        expect(
-            lineToString({
+    describe('lineToString', () => {
+        it('Should write a line from tag and value', () => {
+            expect(
+                lineToString({
+                    level: 2,
+                    tag: 'NAME',
+                    value: 'GEDCOM Specification',
+                }),
+            ).toEqual('2 NAME GEDCOM Specification');
+        });
+
+        it('Should write a line from xref and tag', () => {
+            expect(
+                lineToString({
+                    level: 0,
+                    xrefId: '@I2@',
+                    tag: 'INDI',
+                }),
+            ).toEqual('0 @I2@ INDI');
+        });
+
+        it('Should write a multiline value with CONT', () => {
+            expect(
+                lineToString({
+                    level: 1,
+                    tag: 'NOTE',
+                    value: 'This is a note field that is\ncontinued on the next line.',
+                }),
+            ).toEqual(`1 NOTE This is a note field that is
+2 CONT continued on the next line.`);
+        });
+    });
+
+    describe('lineFromString', () => {
+        it('Should parse tag + value', () => {
+            expect(lineFromString('2 NAME GEDCOM Specification')).toEqual({
                 level: 2,
                 tag: 'NAME',
                 value: 'GEDCOM Specification',
-            }),
-        ).toEqual('2 NAME GEDCOM Specification');
-    });
+            });
+        });
 
-    it('Should write a line from xref and tag', () => {
-        expect(
-            lineToString({
-                level: 0,
-                xrefId: '@I2@',
-                tag: 'INDI',
-            }),
-        ).toEqual('0 @I2@ INDI');
-    });
-
-    it('Should write a multiline value with CONT', () => {
-        expect(
-            lineToString({
+        it('Should parse weird ADOP line', () => {
+            expect(lineFromString('1 ADOP ')).toEqual({
                 level: 1,
-                tag: 'NOTE',
-                value: 'This is a note field that is\ncontinued on the next line.',
-            }),
-        ).toEqual(`1 NOTE This is a note field that is
-2 CONT continued on the next line.`);
-    });
+                tag: 'ADOP',
+                value: '',
+            });
 
-    it('Should parse tag + value', () => {
-        expect(lineFromString('2 NAME GEDCOM Specification')).toEqual({
-            level: 2,
-            tag: 'NAME',
-            value: 'GEDCOM Specification',
-        });
-    });
-
-    it('Should parse weird ADOP line', () => {
-        expect(lineFromString('1 ADOP ')).toEqual({
-            level: 1,
-            tag: 'ADOP',
-            value: '',
+            expect(lineFromString('1 ADOP')).toEqual({
+                level: 1,
+                tag: 'ADOP',
+            });
         });
 
-        expect(lineFromString('1 ADOP')).toEqual({
-            level: 1,
-            tag: 'ADOP',
+        it('Should parse FAM line with ident', () => {
+            expect(lineFromString('0 @F1@ FAM')).toEqual({
+                level: 0,
+                xrefId: '@F1@',
+                tag: 'FAM',
+            });
         });
-    });
 
-    it('Should parse FAM line with ident', () => {
-        expect(lineFromString('0 @F1@ FAM')).toEqual({
-            level: 0,
-            xrefId: '@F1@',
-            tag: 'FAM',
-        });
-    });
-
-    it('Should parse TRLR', () => {
-        expect(lineFromString('0 TRLR')).toEqual({
-            level: 0,
-            tag: 'TRLR',
+        it('Should parse TRLR', () => {
+            expect(lineFromString('0 TRLR')).toEqual({
+                level: 0,
+                tag: 'TRLR',
+            });
         });
     });
 });
