5d8c61d313c89521672f5b5114a7ced4988da608
Jeff VanDyke <jeffvd.a216@gmail.com>
2022-02-15 20:41:20 -0500

    Add writer for strings containing newlines

--- src/gedcom/line.spec.ts
+++ src/gedcom/line.spec.ts
@@ -12,11 +12,24 @@ describe('gedcom/line ', () => {
     });
 
     it('Should write a line from xref and tag', () => {
-        expect(lineToString({
-            level: 0,
-            xrefId: '@I2@',
-            tag: 'INDI',
-        })).toEqual('0 @I2@ INDI');
+        expect(
+            lineToString({
+                level: 0,
+                xrefId: '@I2@',
+                tag: 'INDI',
+            }),
+        ).toEqual('0 @I2@ INDI');
+    });
+
+    it('Should write a multiline value with CONT', () => {
+        expect(
+            lineToString({
+                level: 1,
+                tag: 'NOTE',
+                value: 'This is a note field that is\ncontinued on the next line.',
+            }),
+        ).toEqual(`1 NOTE This is a note field that is
+2 CONT continued on the next line.`);
     });
 
     it.skip('Should parse tag + value', () => {
--- src/gedcom/line.ts
+++ src/gedcom/line.ts
@@ -1,5 +1,11 @@
 import { XrefId } from './types';
 
+/** Roughly, a gedcom line is a line of gedcom (value
+ * may include newlines) that can be joined with other
+ * lines to make a nearly complete GEDCOM file. CONT
+ * and CONC lines are special, and should be combined
+ * with their predecessors before being used. */
+
 export type GedcomLine = {
     level: number;
     xrefId?: XrefId;
@@ -18,6 +24,12 @@ export function lineToString({
     value,
 }: GedcomLine): string {
     const xrefStr = xrefId ? xrefId + ' ' : '';
-    const valueStr = value ? ' ' + value : '';
+
+    const valueStr = value === undefined
+        ? ''
+        : ' ' +
+          (value.includes('\n')
+              ? value.split('\n').join(`\n${level + 1} CONT `)
+              : value);
     return `${level} ${xrefStr}${tag}${valueStr}`;
 }
